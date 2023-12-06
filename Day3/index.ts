e4036af1c943a696e8c41d3d05fd4c5f007a6431
EliasFredriksson <elias.e.fredriksson@gmail.com>
2023-12-06 21:11:08 +0100

    finished Day 3

--- Day3/index.ts
+++ Day3/index.ts
@@ -5,30 +5,27 @@ type Pos = {
   y: number;
 };
 
-const DIGITS: string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const isStar = (char: string): boolean => char.length === 1 && char === "*";
 
-const getSymbols = (input: string): string[] => {
-  return input.split("\r\n").reduce((acc, row) => {
-    const filtered = row
-      .split("")
-      .filter((value) => !DIGITS.includes(value) && value !== ".");
-    return [...new Set([...acc, ...filtered])];
-  }, []);
-};
+const isSymbol = (char: string): boolean =>
+  char.length === 1 && !char.match(/\d+/) && char !== ".";
 
-const getAdjecent = (pos: Pos, partNumber: string, data: string[][]): Pos[] => {
-  const MAX_X = data[0].length;
-  const MAX_Y = data.length;
+const getAdjecent = (
+  pos: Pos,
+  partNumberLength: number,
+  maxX: number,
+  maxY: number
+): Pos[] => {
   const adjecentPositions: Pos[] = [];
   const isAtLeftEdge = pos.x === 0;
-  const isAtRightEdge = pos.x + partNumber.length === MAX_X;
+  const isAtRightEdge = pos.x + partNumberLength === maxX;
   const startIndex = isAtLeftEdge ? 0 : -1;
-  const endIndex = isAtRightEdge ? partNumber.length : partNumber.length + 1;
+  const endIndex = isAtRightEdge ? partNumberLength : partNumberLength + 1;
   for (let xIndex = startIndex; xIndex < endIndex; xIndex++) {
     const finalX = pos.x + xIndex;
-    if (finalX < 0 || finalX > MAX_X) continue;
+    if (finalX < 0 || finalX > maxX) continue;
     const isValidNorthPos = pos.y - 1 >= 0;
-    const isValidSouthPos = pos.y + 1 < MAX_Y;
+    const isValidSouthPos = pos.y + 1 < maxY;
     if (isValidNorthPos) {
       adjecentPositions.push({
         x: pos.x + xIndex,
@@ -50,54 +47,94 @@ const getAdjecent = (pos: Pos, partNumber: string, data: string[][]): Pos[] => {
   }
   if (!isAtRightEdge) {
     adjecentPositions.push({
-      x: pos.x + partNumber.length,
+      x: pos.x + partNumberLength,
       y: pos.y,
     });
   }
   return adjecentPositions;
 };
 
-const isDigit = (char: string): boolean => DIGITS.includes(char);
-
 const part1 = (input: string): number => {
-  const data = input.split("\r\n").map((row) => row.split(""));
-  const MAX_X = data[0].length;
-  const MAX_Y = data.length;
-  const SYMBOLS = getSymbols(input);
-  let totalSumOfParts = 0;
-  let charOffset = 0;
-  for (let y = 0; y < MAX_Y; y++) {
-    for (let x = 0; x < MAX_X; x++) {
-      const char = data[y][x];
-      if (charOffset === 0) {
-        if (isDigit(char)) {
-          let charsOfPartNumber = "";
-          let currentX = x;
-          while (isDigit(data[y][currentX])) {
-            const test = data[y][currentX];
-            charsOfPartNumber += test;
-            currentX++;
-          }
-          const adjecentPositions = getAdjecent(
-            { x, y },
-            charsOfPartNumber,
-            data
-          );
-          const hasSymbol = adjecentPositions.some((pos) => {
-            return SYMBOLS.includes(data[pos.y][pos.x]);
-          });
-          if (hasSymbol) totalSumOfParts += parseInt(charsOfPartNumber);
-          charOffset = charsOfPartNumber.length;
-        }
-      } else {
-        charOffset--;
+  const rows = input.split("\r\n");
+  const MAX_Y = rows.length;
+  let totalSumOfParts: number = 0;
+  for (let i = 0; i < rows.length; ) {
+    const match = rows[i].match(/\d+/);
+    if (match) {
+      const MAX_X = rows[i].length;
+      const { 0: partNumber, index: rowIndex } = match;
+      // Get positions around the found partNumber.
+      const adjecentPositions = getAdjecent(
+        {
+          x: rowIndex,
+          y: i,
+        },
+        partNumber.length,
+        MAX_X,
+        MAX_Y
+      );
+      // Check if any of the positions contain a symbol
+      const hasSymbol = adjecentPositions.some((pos) =>
+        isSymbol(rows[pos.y][pos.x])
+      );
+      if (hasSymbol) {
+        totalSumOfParts += Number(partNumber);
       }
+      // Replace found full number with "." dots.
+      rows[i] = rows[i].replace(partNumber, ".".repeat(partNumber.length));
+    } else {
+      // if we did not find any numbers on current row, continue to next row
+      i++;
     }
   }
   return totalSumOfParts;
 };
 
-const part2 = () => {};
+const part2 = (input: string): number => {
+  const rows = input.split("\r\n");
+  const MAX_Y = rows.length;
+  const gears: Record<string, number[]> = {};
+  for (let i = 0; i < rows.length; ) {
+    // A regex pattern to find the first matched full number on row.
+    const match = rows[i].match(/\d+/);
+    if (match) {
+      const MAX_X = rows[i].length;
+      const { 0: partNumber, index: rowIndex } = match;
+      // Get positions around the found partNumber.
+      const adjecentPositions = getAdjecent(
+        {
+          x: rowIndex,
+          y: i,
+        },
+        partNumber.length,
+        MAX_X,
+        MAX_Y
+      );
+      // Find the position containing a "*" (if there is one)
+      const starPos = adjecentPositions.find((pos) =>
+        isStar(rows[pos.y][pos.x])
+      );
+      if (starPos) {
+        // If we found a "*", add the partNumber to that "*"s numbers
+        const key = `${starPos.y}-${starPos.x}`;
+        if (!gears[key]) gears[key] = [];
+        gears[key].push(Number(partNumber));
+      }
+      // Replace found full number with "." dots.
+      rows[i] = rows[i].replace(partNumber, ".".repeat(partNumber.length));
+    } else {
+      // if we did not find any numbers on current row, continue to next row
+      i++;
+    }
+  }
+  return Object.values(gears).reduce((total, partNumbers) => {
+    if (partNumbers.length === 2) {
+      const [p1, p2] = partNumbers;
+      return total + p1 * p2;
+    }
+    return total;
+  }, 0);
+};
 
 utils.log({
   part1: utils.run(part1, "Day3"),

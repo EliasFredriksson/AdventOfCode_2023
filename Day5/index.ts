b883d7a412e8f9d27763bcbd236d98d165ba95fe
EliasFredriksson <elias.e.fredriksson@gmail.com>
2023-12-07 16:11:43 +0100

    updated

--- Day5/index.ts
+++ Day5/index.ts
@@ -6,21 +6,21 @@ type MapRecord = {
   length: number;
 };
 
-const part1 = (input: string): number => {
-  const [seedsRaw, _, ...mapsRaw] = input.split("\r\n");
+type Seed = {
+  min: number;
+  max: number;
+  range: number;
+};
 
+const getMapsRecord = (mapsRaw: string[]): Record<string, MapRecord[]> => {
   let currentKey: string = null;
-  const mapsRecord = mapsRaw.reduce<Record<string, MapRecord[]>>((acc, row) => {
-    if (!row.length) {
-      return acc;
-    }
-
+  return mapsRaw.reduce<Record<string, MapRecord[]>>((acc, row) => {
+    if (!row.length) return acc;
     if (row.length > 0 && !row.match(/\d+/)) {
       currentKey = row.split(" ")[0];
       acc[currentKey] = [];
       return acc;
     }
-
     const [dest, start, length] = row.split(" ");
     acc[currentKey].push({
       dest: parseInt(dest),
@@ -29,12 +29,13 @@ const part1 = (input: string): number => {
     });
     return acc;
   }, {});
+};
 
-  const seeds = seedsRaw.split(":")[1].trim().split(" ");
-
-  const lowestLocationNumber = seeds.reduce<number>((lowestNum, seed) => {
-    const parsedSeed = parseInt(seed);
-
+const getLowestLocationNumber = (
+  seeds: string[],
+  mapsRecord: Record<string, MapRecord[]>
+): number => {
+  return seeds.reduce<number>((lowestNum, seed) => {
     const seedAfterGoingThroughAllMaps = Object.keys(mapsRecord).reduce(
       (seed, recordKey) => {
         const records = mapsRecord[recordKey];
@@ -54,22 +55,88 @@ const part1 = (input: string): number => {
             }
           }
         });
-
         return mappedSeed;
       },
-      parsedSeed
+      parseInt(seed)
     );
-
     return lowestNum
       ? Math.min(lowestNum, seedAfterGoingThroughAllMaps)
       : seedAfterGoingThroughAllMaps;
   }, null);
+};
 
-  return lowestLocationNumber;
+const getLowestLocationNumber2 = (
+  seeds: Seed[],
+  mapsRecord: Record<string, MapRecord[]>
+): number => {
+  return seeds.reduce<number>((lowestNum, seed) => {
+    const seedAfterGoingThroughAllMaps = Object.keys(mapsRecord).reduce(
+      (seed, recordKey) => {
+        const records = mapsRecord[recordKey];
+        let mappedSeed: number = seed;
+        let foundMatch = false;
+        records.forEach((record) => {
+          const isInRange =
+            seed >= record.start && seed <= record.start + record.length;
+          if (isInRange) {
+            const differenceFromStart = seed - record.start + record.length;
+            const differenceFromDest = seed - record.dest + record.length;
+            const difference = differenceFromStart - differenceFromDest;
+            if (!foundMatch) {
+              mappedSeed = seed + difference;
+              foundMatch = true;
+              return;
+            }
+          }
+        });
+        return mappedSeed;
+      },
+      seed.max
+    );
+    return lowestNum
+      ? Math.min(lowestNum, seedAfterGoingThroughAllMaps)
+      : seedAfterGoingThroughAllMaps;
+  }, null);
+};
+
+const part1 = (input: string): number => {
+  const [seedsRaw, _, ...mapsRaw] = input.split("\r\n");
+
+  const mapsRecord = getMapsRecord(mapsRaw);
+
+  const seeds = seedsRaw.split(":")[1].trim().split(" ");
+
+  return getLowestLocationNumber(seeds, mapsRecord);
 };
 
 const part2 = (input: string): number => {
-  return 2;
+  const [seedsRaw, _, ...mapsRaw] = input.split("\r\n");
+
+  const mapsRecord = getMapsRecord(mapsRaw);
+
+  const seeds = seedsRaw.split(":")[1].trim().split(" ");
+
+  console.log({ seeds });
+
+  let seedsFromRanges: Seed[] = [];
+  while (seeds.length) {
+    const start = parseInt(seeds.shift());
+    const range = parseInt(seeds.shift());
+
+    console.log({ start, range });
+
+    seedsFromRanges.push({
+      min: start,
+      max: start + range,
+      range,
+    });
+  }
+
+  console.log(seedsFromRanges);
+
+  const lowestLocationNumber = getLowestLocationNumber(seeds, mapsRecord);
+
+  return lowestLocationNumber;
 };
 
 utils.log({
